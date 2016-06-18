813032f66b6c06fca8dfde34aca170a176a9b463
DukeBailey <bsdusa11@BSD-11.local>
2016-06-18 11:12:32 -0400

    slither.io uploaded codepen
    cool
    c for constipated
    o for over rated
    o for out of style
    l for loser

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function foridiots() {
+  confirm("you have a freaking virus take BSD PROGRAMING to learn how to fix it:");
+  go()
+}
+
+function go() {
+  var bacon = confirm("y=mx+b")
+  alert(bacon);
+  if (bacon) {
+    alert("bacon is TASTY")
+  } else{alert("you are stuped for not going to the  bacon page IDIOT")}
+}
